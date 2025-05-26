4ae043aa48e0662733210cf44f5e218c6717ceae
42Alberto <albertomunozgarcia.dev@gmail.com>
2025-05-26 12:30:13 +0200

    feat(interceptor): enhance authInterceptor to manage specific routes and improve error handling

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,25 +1,32 @@
-import {
-  HttpHandlerFn,
-  HttpInterceptorFn,
-  HttpRequest,
-} from '@angular/common/http';
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { TokenService } from '../services/token.service';
 import { catchError } from 'rxjs/operators';
 import { inject } from '@angular/core';
 import { throwError } from 'rxjs';
 
+/**
+ * HTTP interceptor to handle authentication
+ * Adds authorization header to requests and handles unauthorized responses
+ */
 export const authInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
+  const tokenService = inject(TokenService);
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  const token = authService.getToken();
+  // Excluir rutas específicas del interceptor
+  if (req.url.includes('/logout') || req.url.includes('/login')) {
+    return next(req);
+  }
 
-  if (token && !req.url.includes('/logout')) {
+  const token = tokenService.getToken();
+
+  if (token) {
     req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
@@ -29,9 +36,9 @@ export const authInterceptor: HttpInterceptorFn = (
 
   return next(req).pipe(
     catchError((error) => {
-      if (error.status === 401) {
+      if (error.status === 401 && !req.url.includes('/logout')) {
         authService.clearSession();
-        if (router.url !== '/login') {
+        if (!router.url.includes('/login')) {
           router.navigate(['/login'], {
             queryParams: { returnUrl: router.url },
           });
