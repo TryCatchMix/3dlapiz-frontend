bea0a8942576585542b2c07b9d2cc876efdb20dc
42Alberto <albertomunozgarcia.dev@gmail.com>
2026-02-04 10:13:49 +0100

    style: update body background color and add rainbow text animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html, ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBg: '#addac0',     // Fondos
+        secondaryBg: '#c6dccf',     // Fondos
+        primaryWithBg: '#FAFAFA', // Fondos con color blanco
+        primaryText: '#cba57f',   // Textos
+        secundaryText: '#ccb094',
+        primaryHover: '#9fceb3',  // Hover botones
+      },
+    },
   },
   plugins: [],
 }
-
