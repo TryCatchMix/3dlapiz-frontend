6a9e4cd3c818f5b4abbd7ca83c34fb7356ac6e65
42Alberto <albertomunozgarcia.dev@gmail.com>
2026-04-29 00:11:06 +0200

    feat(product-model): update Product interface to include youtube_url and unpainted_price

--- src/app/core/models/product.model.ts
+++ src/app/core/models/product.model.ts
@@ -6,11 +6,15 @@ export interface ProductImage {
   updated_at: string | null;
 }
 
+export type ProductVariant = 'painted' | 'unpainted';
+
 export interface Product {
   id: number;
   name: string;
   description: string;
+  youtube_url: string | null;
   price: string;
+  unpainted_price: string | null;
   stock: number;
   created_at: string | null;
   updated_at: string | null;
