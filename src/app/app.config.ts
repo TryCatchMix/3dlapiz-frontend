9362177ce2f9e75c13a300b906a36dcf48434e04
42Alberto <albertomunozgarcia.dev@gmail.com>
2026-06-17 11:54:38 +0200

    feat(app-config): add CurrencyService initialization and update provider structure fix(index.html): correct favicon path for consistency refactor(main.ts): reorganize imports and register additional locale data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideAppInitializer, provideZoneChangeDetection, inject } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { CartService } from './core/services/cart.service';
+import { CurrencyService } from './core/services/currency.service';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withComponentInputBinding()),  provideHttpClient(withInterceptors([authInterceptor])), CartService]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    CartService,
+    provideAppInitializer(() => {
+      const cs = inject(CurrencyService);
+      cs.init();
+    }),
+  ],
 };
