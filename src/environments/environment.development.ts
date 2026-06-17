5f390fd57d47fdab502f1ae4c58c23d70b89bcbf
42Alberto <albertomunozgarcia.dev@gmail.com>
2026-06-18 00:36:10 +0200

    Add environment files to repo

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  API_URL: 'http://127.0.0.1:8000/api',
+  STATIC_URL: 'https://staging.3dlapiz.com',
+  stripePublicKey:
+    'pk_test_51TN9vVHuZ54vtZeaSbLNjGfVnVdOkYABcpLeI35nYouChqtVSLQAux9T2tZnB6JARBnWxwShxOvUD1lO0bY3ZL31003p0hyKnT',
+  googleClientId: '',
+  ENCRYPTION_KEY: 'your-encryption-key-here',
+};
