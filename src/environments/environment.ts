4dcfc35046e22b1303f8125dc8c2efa1b1393c5f
42Alberto <albertomunozgarcia.dev@gmail.com>
2026-06-23 21:44:04 +0200

    feat: update Stripe public key to test version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   API_URL: 'https://3dlapiz.com/api',
   STATIC_URL: 'https://3dlapiz.com',
   stripePublicKey:
-    'pk_live_51TN9vIH3oCSBZqEcAJn38exr6XdEBhcheyJzsKEc2uOMjHSr4aEsqzYusVhnQBTKAj9GICcg2MSWJyEhXbF7Ou3900j9JPBc1x',
+    'pk_test_51TN9vVHuZ54vtZeaSbLNjGfVnVdOkYABcpLeI35nYouChqtVSLQAux9T2tZnB6JARBnWxwShxOvUD1lO0bY3ZL31003p0hyKnT',
   googleClientId: '',
   ENCRYPTION_KEY: 'your-encryption-key-here',
 };
